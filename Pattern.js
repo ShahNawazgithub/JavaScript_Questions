e6532706aa7ca74f32e95b137b8eb1b153c8800f
Shah Nawaz <117187000+ShahNawazgithub@users.noreply.github.com>
2022-12-27 17:15:38 +0530

    first commit

---
+++ Pattern.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 5; i++) {
+    //j+1, 1,2,3,4,5
+for (let j = 0; j < i+1; j++) {
+        console.log(i,j)
+    
+}    
+}
+
+
---
+++ PositiveOrNegativeNumber.js
@@ -0,0 +1,20 @@
+// var a =10
+// let num=a>0?"Positive":"Negative"
+// console.log(num);
+
+
+// var a= parseInt(prompt("Enter the Number"))
+// let num=a>0?"Positive":"Negative"
+// document.write(`Number is ${num}`)
+
+// var a= parseInt(prompt("Enter the Number"))
+// if(a>0){
+//     document.write(`${a} is Positive`)
+// }
+// else if(a<0){
+//     document.write(`${a} is Negative`)
+// }
+// else
+//     document.write(`${a} is Neighter Positive nor Negative`)
+
+
