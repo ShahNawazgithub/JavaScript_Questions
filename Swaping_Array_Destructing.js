ba6ec65da1c728cf9b43abbe34862ec875ebdfe3
Shah Nawaz <117187000+ShahNawazgithub@users.noreply.github.com>
2023-01-16 16:14:19 +0530

    commit

---
+++ Swaping_Array_Destructing.js
@@ -0,0 +1,7 @@
+var a = 1
+var b = 2
+console.log(`Before Swaping \n a = ${a} \n b = ${b}`);
+// console.log(`Before Swaping b = ${b}`);
+let array =[a,b]=[b,a]
+console.log(`After Swaping \n a = ${a} \n b = ${b}`);
+// console.log(`After Swaping b = ${b}`);
